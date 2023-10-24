f0d348f7015833c782c6c940e9ad2205b62fa5b4
dev <dev@dev.com>
2023-10-24 20:54:11 -0300

    chore: update base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://34.83.97.43/api/v1',
+    baseUrl: 'http://34.82.130.101/api/v1',
     mapKey: 'pk.eyJ1IjoiYW50b25pb2Zkc24iLCJhIjoiY2tyZzhtOWRzNDdhODJvbjNnNHgxbGQzOSJ9.rvPTr-FQ4-GNejm5TU1iQg'
 };
