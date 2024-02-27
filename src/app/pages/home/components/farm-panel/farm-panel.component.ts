0fa26a1a0c1178bffb6837a74c41b97589078fe7
dev <dev@dev.com>
2024-02-27 16:34:27 -0300

    feat: download farms as excel

--- src/app/pages/home/components/farm-panel/farm-panel.component.ts
+++ src/app/pages/home/components/farm-panel/farm-panel.component.ts
@@ -4,6 +4,8 @@ import { Farm } from 'src/app/core/models/farm.model';
 import { ProducerService } from 'src/app/core/services/producer.service';
 import { DrawerService } from '../../services/drawer.service';
 import { MapService } from '../../services/map.service';
+import { first } from 'rxjs/operators';
+import { LoadingService } from 'src/app/core/services/loading.service';
 
 @Component({
     selector: 'app-farm-panel',
@@ -13,14 +15,15 @@ import { MapService } from '../../services/map.service';
 export class FarmPanelComponent {
     isCitiesEnabled$ = this.producerService.isCitiesEnabled$
     isCitiesLoading$ = this.producerService.isCitiesLoading$
-    
+
     isFarmsLoading$ = this.producerService.isFarmsLoading$
     farms$ = this.producerService.farmSelection$
 
     constructor(
         private drawerService: DrawerService,
         private mapService: MapService,
-        private producerService: ProducerService
+        private producerService: ProducerService,
+        private loadingService: LoadingService
     ) { }
 
     setCitiesEnabled(enabled: boolean) {
@@ -33,6 +36,24 @@ export class FarmPanelComponent {
 
     selectFarms(farms: Farm[]) {
         this.producerService.selectFarms(farms.map(farm => farm._id))
+    }
+
+    downloadFarms() {
+        this.loadingService.on()
+        this.producerService.downloadFarmsAsExcel()
+            .pipe(first())
+            .subscribe(response => {
+                this.downLoadFile(response.body, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                this.loadingService.off()
+            })
+    }
+
+    private downLoadFile(data: any, type: string) {
+        const blob = new Blob([data], { type })
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(blob)
+        link.download = 'propriedades.xlsx'
+        link.click()
     }
 
     moveToFarm(farm: Farm) {
